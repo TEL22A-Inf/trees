bac61d33602d72f9029b8688e038fa63d5ed3b06
Reiner Hüchting <dev@reinerhuechting.de>
2023-05-11 09:42:53 +0200

    Add tasks with comments.

---
+++ binsearchtree/tree_aufgaben.go
@@ -0,0 +1,80 @@
+package binsearchtree
+
+/*
+Diese Datei enthält die Aufgaben für den Binärbaum.
+Es geht darum, weitere Methoden für den Binärbaum zu implementieren.
+
+Beachten Sie, dass diese Datei sowohl Methoden für den Baum als auch für die Elemente enthält.
+*/
+
+// GetMinNode gibt das Element mit dem kleinsten Wert zurück.
+// Wenn der Baum leer ist, wird nil zurückgegeben.
+func (t *Element) GetMinNode() *Element {
+	// TODO
+	return nil
+}
+
+// GetMaxNode gibt das Element mit dem größten Wert zurück.
+// Wenn der Baum leer ist, wird nil zurückgegeben.
+func (t *Element) GetMaxNode() *Element {
+	// TODO
+	return nil
+}
+
+// RemoveValue entfernt ein Element mit dem Wert value aus dem Baum.
+// Wenn das Element nicht gefunden wird, passiert nichts.
+func (t *Tree) RemoveValue(value int) {
+	// TODO
+}
+
+// NodeCount gibt die Anzahl der Elemente im Baum zurück.
+func (t *Tree) NodeCount() int {
+	// TODO
+	return 0
+}
+
+// IsSearchTree gibt true zurück, wenn der Baum ein Binärbaum ist.
+func (t *Tree) IsSearchTree() bool {
+	// TODO
+	return false
+}
+
+// Height gibt die Höhe des Baums zurück.
+func (t *Element) Height() int {
+	// TODO
+	return 0
+}
+
+// BalanceFactor gibt den Balancefaktor des Knotens zurück.
+func (e *Element) BalanceFactor() int {
+	// TODO
+	return 0
+}
+
+// RotateLeft rotiert den Baum nach links.
+// Die Funktion erwartet die Wurzel des (Teil-)Baums und gibt die neue Wurzel zurück.
+func RotateLeft(root *Element) *Element {
+	// TODO
+	return nil
+}
+
+// RotateRight rotiert den Baum nach rechts.
+// Die Funktion erwartet die Wurzel des (Teil-)Baums und gibt die neue Wurzel zurück.
+func RotateRight(root *Element) *Element {
+	// TODO
+	return nil
+}
+
+// RotateLeftRight führt eine Doppelrotation nach links aus.
+// Die Funktion erwartet die Wurzel des (Teil-)Baums und gibt die neue Wurzel zurück.
+func RotateLeftRight(root *Element) *Element {
+	// TODO
+	return nil
+}
+
+// RotateRightLeft führt eine Doppelrotation nach rechts aus.
+// Die Funktion erwartet die Wurzel des (Teil-)Baums und gibt die neue Wurzel zurück.
+func RotateRightLeft(root *Element) *Element {
+	// TODO
+	return nil
+}
